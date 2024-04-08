d899120cd89683f68d819fb4fff60db56df7dfb4
akshat kumar <152690395+AkshatforGithub@users.noreply.github.com>
2024-04-08 15:34:28 +0530

    Initial commit

---
+++ js-utils/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Textfile from './components/Textfile'
+
+function App() {
+  return (
+    <>
+    <Navbar title="BLOG" home="HOME" about="About Author" />
+    <Textfile/>
+    </>
+  )
+}
+
+export default App;
---
+++ js-utils/src/components/Textfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function content() {
+  return (
+    <form>
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">text area </label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+    </div>
+  </form>
+  )
+}
