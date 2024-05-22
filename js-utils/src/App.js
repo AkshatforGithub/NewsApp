6ff84c2cf7510c13220d625c875447f75585dfd8
akshat kumar <152690395+AkshatforGithub@users.noreply.github.com>
2024-05-22 23:09:53 +0530

    added alert funtion to the app

--- js-utils/src/App.js
+++ js-utils/src/App.js
@@ -1,17 +1,17 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import Textfile from './components/Textfile'
-
+// import About from './components/About';
+import Textfile from './components/Textfile';
 function App() {
   return (
     <>
-    <Navbar title="Text Converter" home="HOME" about="About" />
+    <Navbar title="Text Converter" home="Home" about="About" />
     <div className="container">
-      <Textfile heading  = "Scribe"/>
+    <Textfile heading  = "Scribe"/>
+    {/* <About/> */}
     </div>
     </>
   )
 }
-
 export default App;
--- js-utils/src/components/Textfile.js
+++ js-utils/src/components/Textfile.js
@@ -1,41 +1,56 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Textfile(props) {
-  
-  const handleUpClick = ()=> {
+  const handleUpClick = () => {
     console.log("upercase was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
-  }
-  const handleOnChange = (event)=>{
+    // props.showalert("converted to uppercase!", "success")
+    toast.success("Converted to uppercase!");
+  };
+  const handleOnChange = (event) => {
     console.log("OnChange");
-    setText(event.target.value)
-  }
+    setText(event.target.value);
+  };
 
-  const [text, setText] = useState('enter your text here');
+  const [text, setText] = useState(""); 
   return (
     <>
+      
       <form>
-        <div className="container">
-          <h1>
-            {props.heading}
-          </h1>
+        <div className="container my-4">
+          <h1>{props.heading}</h1>
           <label for="exampleFormControlTextarea1"> </label>
           <textarea
-            className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8" >
-            </textarea>
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="exampleFormControlTextarea1"
+            rows="8"
+            placeholder="enter your text here ..."
+          ></textarea>
         </div>
       </form>
-          <button className="btn btn-primary" onClick={handleUpClick}>convert to uppercase</button>
-          <div className="container my-4">
-            <h1>The sentence contains a certain quantity of words</h1> 
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-           </div>
-           <div className="container">
-            <h1>time taken in reading your text </h1>
-             <p>{0.008 * text.split(" ").length}minutes</p>
-           </div>
+      <button className="btn btn-primary ms-2" onClick={handleUpClick}>
+        convert to uppercase
+      </button>
+      <div className="container my-4">
+        <h1>The sentence contains a certain quantity of words</h1>
+        <p>
+          {text ? text.trim().split(/\s+/).length : 0} words and{" "}
+          {text.trim().length} characters.
+        </p>
+      </div>
+      <div className="container">
+        <h1>Time taken in reading your text </h1>
+        <p>
+          {text ? (0.15 * text.split("").length).toPrecision(1) : 0} seconds
+        </p>
+      </div>
+      <ToastContainer />
     </>
   );
-  
 }
