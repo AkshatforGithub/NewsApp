c135bbb3d5b113c8f5e141569466c24dbbd7949e
akshat kumar <152690395+AkshatforGithub@users.noreply.github.com>
2024-05-29 20:25:38 +0530

    added functionality in the website NewsMonkeyu
    added features like previous and next button also made the web page more funttional.

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let{title,description,imageUrl}= this.props;
+    let{title,description,imageUrl,url}= this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem;"}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <img src={!imageUrl?"https://th.bing.com/th/id/OIP.L1E87XXRZaz30nUfxM48GQHaEK?w=315&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/cards" className="btn btn-primary">Read More</a>
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}....</p>
+    <a href={`${url}`} target="_blank" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
 
