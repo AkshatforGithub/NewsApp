83d1fdfba79e8e915ecf4ea9a09d07683c6f52c4
akshat kumar <152690395+AkshatforGithub@users.noreply.github.com>
2024-05-30 11:41:21 +0530

    added weather widget in the website

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -1,10 +1,127 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import './Navbar.css'; 
+
+// export class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#fb607f', color: 'black' }}>
+//           <div className="container-fluid">
+//             <a className="navbar-brand" href="/">
+//               NewsMoMMy
+//             </a>
+//             <button
+//               className="navbar-toggler"
+//               type="button"
+//               data-bs-toggle="collapse"
+//               data-bs-target="#navbarSupportedContent"
+//               aria-controls="navbarSupportedContent"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation"
+//             >
+//               <span className="navbar-toggler-icon"></span>
+//             </button>
+//             <div
+//               className="collapse navbar-collapse"
+//               id="navbarSupportedContent"
+//             >
+//               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                 <li className="nav-item">
+//                   <a
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     href="/home"
+//                   >
+//                     Home
+//                   </a>
+//                 </li>
+//                 <li className="nav-item">
+//                   <a className="nav-link" href="/about">
+//                     About
+//                   </a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Navbar;
+// import React, { Component } from "react";
+// import './Navbar.css'; 
+
+// export class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <nav className="navbar navbar-expand-lg custom-navbar" style={{ backgroundColor: '#fb607f', color: 'black' }}>
+//           <div className="container-fluid">
+//             <a className="navbar-brand" href="/">
+//               NewsMoMMy
+//             </a>
+//             <button
+//               className="navbar-toggler"
+//               type="button"
+//               data-bs-toggle="collapse"
+//               data-bs-target="#navbarSupportedContent"
+//               aria-controls="navbarSupportedContent"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation"
+//             >
+//               <span className="navbar-toggler-icon"></span>
+//             </button>
+//             <div
+//               className="collapse navbar-collapse"
+//               id="navbarSupportedContent"
+//             >
+//               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                 <li className="nav-item">
+//                   <a
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     href="/home"
+//                   >
+//                     Home
+//                   </a>
+//                 </li>
+//                 <li className="nav-item">
+//                   <a className="nav-link" href="/about">
+//                     About
+//                   </a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Navbar;
+
+import React, { Component } from 'react';
+import './Navbar.css';
 
 export class Navbar extends Component {
+  componentDidMount() {
+    // Create a script element
+    const script = document.createElement('script');
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.defer = true;
+    script.dataset.useServiceCore = true;
+
+    // Append the script to the document body
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <nav className="navbar navbar-expand-lg custom-navbar" style={{ backgroundColor: '#fb607f', color: 'black' }}>
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
               NewsMoMMy
@@ -20,17 +137,10 @@ export class Navbar extends Component {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/home"
-                  >
+                  <a className="nav-link active" aria-current="page" href="/home">
                     Home
                   </a>
                 </li>
@@ -40,9 +150,12 @@ export class Navbar extends Component {
                   </a>
                 </li>
               </ul>
+              <div className="elfsight-app-550dcbc0-5195-4fc9-add7-e6bfc4bb61fb" data-elfsight-app-lazy></div>
             </div>
           </div>
         </nav>
+        {/* Elfsight widget */}
+        {/* <div className="elfsight-app-550dcbc0-5195-4fc9-add7-e6bfc4bb61fb" data-elfsight-app-lazy></div> */}
       </div>
     );
   }
--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,49 +1,95 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Lottie from "react-lottie";
+import scrollAnim from "../assets/anim.json"
 
 export class News extends Component {
- 
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
-      page:1
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=839c8e4abd9941429d81463a91417455"
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=839c8e4abd9941429d81463a91417455";
     let data = await fetch(url);
-    let parsedData = await data.json()
+    let parsedData = await data.json();
     // console.log(parsedData)
     // console.log(data);
-    this.setState({articles:parsedData.articles})
+    this.setState({ articles: parsedData.articles });
   }
 
   render() {
     return (
-      <div className="my-3">
+      <div
+        className="py-3"
+        style={{ backgroundColor: "rgba(255,234,227,0.3)" }}
+      >
         <div className="container my-4">
           <h1>NewsMommY- TopHeadlines</h1>
           <div className="my-4">
             <div className="row">
               {this.state.articles.map((element) => {
-                return <div className="col-md-4" key={element.url}>
-                  <NewsItem 
-                    title={element.title?element.title.slice(0,40) :""}
-                    description={element.description?element.description.slice(0,100):""}
-                    imageUrl={element.urlToImage}
-                    url={element.url}
-                  />
-                </div>
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 40) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 100)
+                          : ""
+                      }
+                      imageUrl={element.urlToImage}
+                      url={element.url}
+                    />
+                  </div>
+                );
               })}
             </div>
+            <div className=" d-flex justify-content-between mt-4">
+              {/* <button type="button" class="btn btn-info " style={{ backgroundColor: '#fb607f', color: 'black',borderColor:'pink'}}> &#8604; Previous</button> */}
+              {/* <button type="button" class="btn btn-info " style={{ backgroundColor: '#fb607f', color: 'black',borderColor:'pink'}}>Next &#8605;</button> */}
+              <button
+                type="button"
+                class="btn btn-info "
+                style={{
+                  backgroundColor: "#fb607f",
+                  color: "black",
+                  borderColor: "pink",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Lottie
+                  options={{ loop: true, animationData: scrollAnim }}
+                  height={40}
+                  style={{ transform: "rotateX(180deg) rotate(180deg)" }}
+                />
+                Previous
+              </button>
+              <button
+                type="button"
+                class="btn btn-info "
+                style={{
+                  backgroundColor: "#fb607f",
+                  color: "black",
+                  borderColor: "pink",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                Next
+                <Lottie
+                  options={{ loop: true, animationData: scrollAnim }}
+                  height={40}
+                />
+              </button>
+            </div>
           </div>
-        <div className=" d-flex justify-content-between">
-        <button type="button" class="btn btn-info "> &#8604; Previous</button>
-        <button type="button" class="btn btn-info ">Next &#8605;</button>
-        </div>
         </div>
       </div>
     );
